769d22ed7b66e60dc4a87ff1ef118e676a291651
정현수 <jung660317@naver.com>
2022-05-14 19:39:24 +0900

    chore: mock 데이터 삭제

--- src/components/LazyLoadingImage.js
+++ src/components/LazyLoadingImage.js
@@ -1,45 +1,4 @@
-const images = [
-  {
-    url: 'https://cdn2.thecatapi.com/images/nG2rFqXai.jpg',
-    id: 'nG2rFqXai',
-  },
-  {
-    url: 'https://cdn2.thecatapi.com/images/c2m.jpg',
-    id: 'c2m',
-  },
-  {
-    url: 'https://cdn2.thecatapi.com/images/blr4iUu-b.jpg',
-    id: 'blr4iUu-b',
-  },
-  {
-    url: 'https://cdn2.thecatapi.com/images/hq.jpg',
-    id: 'hq',
-  },
-  {
-    url: 'https://cdn2.thecatapi.com/images/tF_6-Beo_.jpg',
-    id: 'tF_6-Beo_',
-  },
-  {
-    url: 'https://cdn2.thecatapi.com/images/avg.jpg',
-    id: 'avg',
-  },
-  {
-    url: 'https://cdn2.thecatapi.com/images/4u3.jpg',
-    id: '4u3',
-  },
-  {
-    url: 'https://cdn2.thecatapi.com/images/MTc3NjkyNg.gif',
-    id: 'MTc3NjkyNg',
-  },
-  {
-    url: 'https://cdn2.thecatapi.com/images/14h.gif',
-    id: '14h',
-  },
-  {
-    url: 'https://cdn2.thecatapi.com/images/Z0X1tZQIt.jpg',
-    id: 'Z0X1tZQIt',
-  },
-];
+import cats from '../mock/cats.js';
 
 class LazyLoadingImage {
   constructor($target) {
@@ -47,7 +6,7 @@ class LazyLoadingImage {
     this.$imageContainer.className = 'big-block';
 
     const template = () =>
-      images
+      cats
         .map(image => {
           return String.raw`
             <div data-url="${image.url}" class="lazy-loading">
