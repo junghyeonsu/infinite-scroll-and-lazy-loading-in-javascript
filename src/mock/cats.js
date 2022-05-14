1ada9f1ba94013f1286f8f6d6c3f15cd32abef95
정현수 <jung660317@naver.com>
2022-05-14 19:38:37 +0900

    chore: 고양이 목업 데이터 생성

---
+++ src/mock/cats.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    url: 'https://cdn2.thecatapi.com/images/nG2rFqXai.jpg',
+    id: 'nG2rFqXai',
+  },
+  {
+    url: 'https://cdn2.thecatapi.com/images/c2m.jpg',
+    id: 'c2m',
+  },
+  {
+    url: 'https://cdn2.thecatapi.com/images/blr4iUu-b.jpg',
+    id: 'blr4iUu-b',
+  },
+  {
+    url: 'https://cdn2.thecatapi.com/images/hq.jpg',
+    id: 'hq',
+  },
+  {
+    url: 'https://cdn2.thecatapi.com/images/tF_6-Beo_.jpg',
+    id: 'tF_6-Beo_',
+  },
+  {
+    url: 'https://cdn2.thecatapi.com/images/avg.jpg',
+    id: 'avg',
+  },
+  {
+    url: 'https://cdn2.thecatapi.com/images/4u3.jpg',
+    id: '4u3',
+  },
+  {
+    url: 'https://cdn2.thecatapi.com/images/MTc3NjkyNg.gif',
+    id: 'MTc3NjkyNg',
+  },
+  {
+    url: 'https://cdn2.thecatapi.com/images/14h.gif',
+    id: '14h',
+  },
+  {
+    url: 'https://cdn2.thecatapi.com/images/Z0X1tZQIt.jpg',
+    id: 'Z0X1tZQIt',
+  },
+];
